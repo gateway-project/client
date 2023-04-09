a9a955b493c490d12c4a4ffb3d6a02568244f714
Jisook93 <leejisook0718@gmail.com>
2023-04-09 21:03:48 +0900

    Feat. Add a-entity with camera

--- src/loaders/middlewares.js
+++ src/loaders/middlewares.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const path = require("path");
 
 const middlewareLoader = async (app) => {
   app.use(logger(app.get("env") === "development" ? "dev" : "combined"));
+  app.use(express.static(path.join(__dirname, "/")));
+  app.use(express.static("public"));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
