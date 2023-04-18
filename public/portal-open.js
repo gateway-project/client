5d5d0d6d2ae4895d0f1142b708115ff34544cce9
Jisook93 <leejisook0718@gmail.com>
2023-04-19 02:42:36 +0900

    Feat. portal appear & owl enter animation

---
+++ public/portal-open.js
@@ -0,0 +1,33 @@
+AFRAME.registerComponent("portal-open", {
+  /*
+랜드마크 클릭하면, 1) 포탈이 뿅 나타남
+2) 그러면 부엉이가 포탈로 날아감
+3) 그러면 해당 랜드마크.html로 주소 이동
+*/
+
+  init: function () {
+    const teleport = document.getElementById("teleport");
+    const owl = document.getElementById("owl2");
+
+    this.showPortal = function (e) {
+      let params = {
+        property: "position",
+        to: {
+          x: 0,
+          y: -1,
+          z: -3,
+        },
+        dur: 4000,
+        easing: "easeInCubic",
+      };
+      teleport.setAttribute("visible", "true");
+      owl.setAttribute("animation", params);
+      console.log("why");
+      owl.addEventListener("animationcomplete", function () {
+        window.location.href = "/usa.html";
+      });
+    };
+
+    this.el.addEventListener("click", this.showPortal);
+  },
+});
